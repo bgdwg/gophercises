cc631fbb2b5d580523fb9cb7f74fb4bdb6fd6302
bgdwg <bgdwg@protonmail.com>
2022-02-08 16:02:18 +0300

    refactor part1

--- 01-quiz-game/main.go
+++ 01-quiz-game/main.go
@@ -5,38 +5,53 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"reflect"
 )
 
-func main() {
-	filenamePtr := flag.String("f", "problems.csv", "filename")
-	flag.Parse()
-	f, err := os.Open(*filenamePtr)
+func readCsvFile(filename string) ([][]string, error) {
+	f, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		return [][]string{}, fmt.Errorf("unable to open %s - %w", filename, err)
 	}
 	defer f.Close()
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
 	if err != nil {
-		panic(err)
+		return [][]string{}, fmt.Errorf("unable to read all records from %s - %w", filename, err)
 	}
-	cntCorrect := 0
-	for i, record := range records {
-		fmt.Printf("Question %d: %s=", i+1, record[0])
+	return records, nil
+}
+
+func startQuizGame(problems [][]string) (int, error) {
+	numCorrect := 0
+	for i, problem := range problems {
+		question, correctAnswer := problem[0], []byte(problem[1])
+		fmt.Printf("Problem #%d: %s = ", i+1, question)
 		reader := bufio.NewReader(os.Stdin)
-		correctAns := []byte(record[1])
-		ans := make([]byte, len(correctAns))
-		_, err := reader.Read(ans)
-		if err != nil {
-			panic(err)
+		answer := make([]byte, len(correctAnswer))
+		if _, err := reader.Read(answer); err != nil {
+			return 0, fmt.Errorf("unable to read your answer")
 		}
-		if reflect.DeepEqual(ans, correctAns) {
-			cntCorrect++
-		} else {
+		if !reflect.DeepEqual(answer, correctAnswer) {
 			break
 		}
+		numCorrect++
 	}
-	fmt.Printf("Result: %d out of %d\n", cntCorrect, len(records))
+	return numCorrect, nil
+}
+
+func main() {
+	filenamePtr := flag.String("f", "problems.csv", "filename")
+	flag.Parse()
+	problems, err := readCsvFile(*filenamePtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := startQuizGame(problems)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("You scored %d out of %d.\n", result, len(problems))
 }
